90493caf56b0fc69275182f4e8123245e86c75ae
anujagargate <gargatean@g.cofc.edu>
2017-09-30 03:48:47 +0000

    finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,34 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+     sort= params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering = {:title=>:asc}
+      @title_header = 'hilite'
+    when 'release_date'
+      ordering = {:release_date=>:asc}
+      @date_header = 'hilite'
+  end
+    
+    @movies = Movie.order(ordering)
+    
+   @all_ratings=Movie.all_ratings
+   @selected_ratings = params[:ratings] ||session[:ratings] || {}
+   
+   
+   if @selected_ratings == {}
+     @selected_ratings = Hash[@all_ratings.map{|rating| [rating,rating]}]
+   end
+   
+   @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+   
+   if params[:sort]!= session[:sort] || params[:ratings]!= session[:ratings]
+    session[:sort] = sort
+    session[:ratings] = @selected_ratings
+    flash.keep
+    redirect_to :sort =>sort, :ratings =>@selected_ratings and return
+   end
   end
 
   def new
